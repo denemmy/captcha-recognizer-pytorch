2eddb48549e2c3fd41462a650f151f7de13ce529
denemmy <denemmy@gmail.com>
2020-02-25 18:00:36 +0300

    update checkpoint

---
+++ scripts/convert_checkpoint.py
@@ -0,0 +1,14 @@
+import torch
+
+
+def convert():
+    checkpoint_path = '/media/denemmy/hdd/tensorboard/captcha/captcha_classification/baseline/007_300k_data_adam_50k_iter/checkpoints/latest_checkpoint.tar'
+    output_path = '/media/denemmy/hdd/tensorboard/captcha/captcha_classification/baseline/007_300k_data_adam_50k_iter/checkpoints/model.pt'
+
+    states = torch.load(checkpoint_path)
+    net_states = states['net']
+    torch.save(net_states, output_path)
+
+
+if __name__ == '__main__':
+    convert()
